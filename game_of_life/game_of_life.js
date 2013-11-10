fc6a9bd68fa01375542e995c466dd0995d03999b
Shreyas Satish <shreyas.satish@gmail.com>
2013-11-10 17:58:30 +0530

    changed gol logic

--- game_of_life/game_of_life.js
+++ game_of_life/game_of_life.js
@@ -1,132 +1,122 @@
-
-
 // This is a JavaScript implementation of the Game of Life - 
 // https://en.wikipedia.org/wiki/Conway's_Game_of_Life
-
 // For the demo, please go to http://shreyas.io/demos/game_of_life/
-
-// Define a starting configuration, like the one below:
-// var startingGrid = [
-//   ['.', '.', '.', '.', '.', '.'],
-//   ['.', '.', '.', '.', '.', '.'],
-//   ['.', '.', 'x', 'x', 'x', '.'],
-//   ['.', '.', '.', '.', '.', '.'],
-//   ['.', '.', '.', '.', '.', '.'],
-//   ['.', '.', '.', '.', '.', '.']
-// ];
-
-// Initiate the grid or the 'world' - 
-// var world = Controller.populateWorld(startingGrid);
-
-// To step through each iteration :
-// world = Controller.updateWorld(world);
-
-
-(function(){
-
+(function () {
   var Controller = {
-    populateWorld: function (startingGrid) {
-      var initialWorld = [];
-      // true is alive
-      var symbolStatusMap = {
-        'x': true,
-        '.': false
-      };
-
-      for (var x = 0; x < startingGrid.length; x++) {
-        for (var y = 0; y < startingGrid[x].length; y++) {
-          if (!initialWorld[x]) {
-            initialWorld[x] = [];
+      populateWorld: function (rows, cols, config) {
+        var initialWorld = [];
+        for (var x = 0; x < rows; x++) {
+          for (var y = 0; y < cols; y++) {
+            if (!initialWorld[x]) {
+              initialWorld[x] = [];
+            }
+            initialWorld[x][y] = new Cell(new Position(x, y), false);
           }
-          initialWorld[x][y] = new Cell(new Position(x, y), symbolStatusMap[startingGrid[x][y]]);
         }
-      };
-      return initialWorld;
-    },
-    updateWorld: function (world) {
-      for (var rowIndex = 0; rowIndex < world.length; rowIndex++) {
-        for (var cellIndex = 0; cellIndex < world[rowIndex].length; cellIndex++) {
-          world[rowIndex][cellIndex].was_alive = world[rowIndex][cellIndex].alive;
-          var aliveNeighboursCount = world[rowIndex][cellIndex].aliveNeighboursCount(world);
-          
-          // Cell becomes/stays alive if too few or too many alive neighbors
-          if ((aliveNeighboursCount < 2) || (aliveNeighboursCount > 3)) {
-            world[rowIndex][cellIndex].alive = false;
-          }
-          // Dead cell comes to life amongst 3 alive neighbours
-          else if (!world[rowIndex][cellIndex].alive && aliveNeighboursCount === 3) {
-            world[rowIndex][cellIndex].alive = true;
-          }
-         
-        };
-      };
-      for (rowIndex = 0; rowIndex < world.length; rowIndex++) {
-        for (cellIndex = 0; cellIndex < world[rowIndex].length; cellIndex++) {
-          world[rowIndex][cellIndex].was_alive = world[rowIndex][cellIndex].alive;
-        }
+        config.map(function (xy) {
+          initialWorld[xy[0]][xy[1]].alive = true;
+        });
+        return initialWorld;
+      },
+      updateWorld: function (world) {
+        var nextGenMap = [];
+        var nextState;
+        var aliveNeighboursCount;
+        world.map(function (row) {
+          row.map(function (cell) {
+            aliveNeighboursCount = cell.aliveNeighboursCount(world);
+            if (aliveNeighboursCount < 2 || aliveNeighboursCount > 3) {
+              nextGenMap.push({
+                x: cell.position.x,
+                y: cell.position.y,
+                nextState: false
+              });
+            }  // Dead cell comes to life amongst 3 alive neighbours
+            else if (!cell.alive && aliveNeighboursCount === 3) {
+              nextGenMap.push({
+                x: cell.position.x,
+                y: cell.position.y,
+                nextState: true
+              });
+            }
+          });
+        });
+        nextGenMap.map(function (val) {
+          world[val.x][val.y].alive = val.nextState;
+        });
+        return world;
       }
-      return world;
-    }
-  }
-
-  // All the cells have to be updated simultaneously. So, at t1, if cell no. 1
-  // has it's 'alive' status changed (status to be represented in t2),
-  // we store the cell 1's status at t1 in the was_alive property,
-  // which allows us to simulate a 'concurrent' process.
-  function Cell (position, alive) {
+    };
+  function Cell(position, alive) {
     this.position = position;
     this.alive = alive && true;
-    this.was_alive = this.alive;
-    console.log(this.alive);
   }
-
-  Cell.prototype.aliveNeighboursCount = function(world) {
-    var neighbors = this.position.neighbors(world.length, world[0].length);
+  Cell.prototype.aliveNeighboursCount = function (world) {
+    var neighbors = this.position.neighbours(world.length, world[0].length);
     var count = 0;
     var position;
     for (var i = 0; i < neighbors.length; i++) {
       position = neighbors[i];
-      if (world[position.x][position.y].was_alive) {
+      if (world[position.x][position.y].alive) {
         count += 1;
       }
-    };
+    }
     return count;
-  }
-
+  };
   function Position(x, y) {
     this.x = x;
     this.y = y;
   }
-
   // The grid system used here is Cartesian.
-  Position.prototype.neighbors = function(rowUpperBound, colUpperBound) {
+  Position.prototype.neighbours = function (rowUpperBound, colUpperBound) {
     var x = this.x;
     var y = this.y;
     var neighborMap = [
-      [x + 1, y],
-      [x, y + 1],
-      [x + 1, y + 1],
-      [x - 1, y + 1],
-      [x - 1, y],
-      [x, y - 1,],
-      [x - 1, y - 1],
-      [x + 1, y - 1]
-    ];
+        [
+          x + 1,
+          y
+        ],
+        [
+          x,
+          y + 1
+        ],
+        [
+          x + 1,
+          y + 1
+        ],
+        [
+          x - 1,
+          y + 1
+        ],
+        [
+          x - 1,
+          y
+        ],
+        [
+          x,
+          y - 1
+        ],
+        [
+          x - 1,
+          y - 1
+        ],
+        [
+          x + 1,
+          y - 1
+        ]
+      ];
     var neighbouringPositions = [];
-
-    var withinBounds = function(x, y) {
-      return (x >= 0) && (y >= 0) && (x < rowUpperBound) && (y < colUpperBound);
-    }
+    var withinBounds = function (x, y) {
+      return x >= 0 && y >= 0 && x < rowUpperBound && y < colUpperBound;
+    };
     for (var i = 0; i < neighborMap.length; i++) {
-      if (withinBounds(neighborMap[i][0], neighborMap[i][1])){
+      if (withinBounds(neighborMap[i][0], neighborMap[i][1])) {
         neighbouringPositions.push(new Position(neighborMap[i][0], neighborMap[i][1]));
       }
-    };
+    }
     return neighbouringPositions;
-  }
-
+  };
   window.Controller = Controller;
-
   // Support for unsupported browsers.
   if (!Array.prototype.forEach) {
     Array.prototype.forEach = function (fn, scope) {
@@ -139,4 +129,20 @@
       }
     };
   }
-}).call(this);
+}.call(this));
+
+// NG Setup
+var app = angular.module('App', []);
+angular.module('App').controller('MainCtrl', function($scope){
+  var slider = [[4,3],[4,4],[4,5]];
+  var glider = [[3,4],[4,4],[5,4],[5,3],[4,2]];
+  var toad = [[4,4],[4,5],[4,6],[5,3],[5,4],[5,5]];
+
+  $scope.cells = Controller.populateWorld(8,8,glider);
+  $scope.toggleStatus = function(cell){
+    cell.alive = !cell.alive;
+  };
+  $scope.runSimulation = function(){
+    $scope.cells = Controller.updateWorld($scope.cells);
+  };
+});
